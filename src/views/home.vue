ef00955f0b4d07a3e00a05493d9a7a59e0dcd600
BQX <b22050106@njupt.edu.cn>
2023-10-14 00:07:43 +0800

    feat bottomNav register

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,5 +1,14 @@
 <template>
   this is home
+  <BottomNav></BottomNav>
 </template>
 <script setup>
+import BottomNav from "../components/bottomNav.vue";
 </script>
+<style>
+.icon{
+    color: #2D328E;
+    height: 40px;
+    width: 40px;
+}
+</style>
